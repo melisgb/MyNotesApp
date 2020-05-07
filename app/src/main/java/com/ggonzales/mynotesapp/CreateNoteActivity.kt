1231391257b7026548ea64ee96ae3baa30be099b
melisgb <gloriamelissa1@hotmail.com>
2020-05-07 20:02:14 +0100

    DB Manager enable to create notes.

--- app/src/main/java/com/ggonzales/mynotesapp/CreateNoteActivity.kt
+++ app/src/main/java/com/ggonzales/mynotesapp/CreateNoteActivity.kt
@@ -1,8 +1,11 @@
 package com.ggonzales.mynotesapp
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.contentcapture.ContentCaptureCondition
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_create_note.*
 
 class CreateNoteActivity : AppCompatActivity() {
@@ -13,6 +16,17 @@ class CreateNoteActivity : AppCompatActivity() {
 
         createNoteButton.setOnClickListener {
             //instead of using a new intent to go back to MainActivity, we finish this activity and will go back.
+            val DBM = DBManager(this)
+            val title = noteTitleEditText.text.toString()
+            val desc = noteDescEditText.text.toString()
+            var values = ContentValues()
+            values.put("title", title)
+            values.put("desc", desc)
+            val ID = DBM.insertNote(values)
+            if(ID > 0){
+                Toast.makeText(this, "Note added", Toast.LENGTH_SHORT).show()
+            }
+            else Toast.makeText(this, "Unable to add note", Toast.LENGTH_SHORT).show()
             finish()
         }
     }
--- app/src/main/java/com/ggonzales/mynotesapp/DBManager.kt
+++ app/src/main/java/com/ggonzales/mynotesapp/DBManager.kt
@@ -1,2 +1,55 @@
 package com.ggonzales.mynotesapp
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.media.projection.MediaProjection
+import android.widget.Toast
+//when trying to access the DB locally you can access C:\Users\[user]\AppData\Local\Android\sdk\platform-tools
+
+class DBManager{
+    val dbName = "NotesDB"
+    val dbTable = "Notes"
+    val id = "ID"
+    val title = "title"
+    val desc = "desc"
+    var dbVersion = 1
+
+    val queryCreateTable = "CREATE TABLE IF NOT EXISTS " + dbTable + " (" + id + " INTEGER PRIMARY KEY, "+
+            title + " TEXT, " + desc + " TEXT );"
+
+    var sqlDB : SQLiteDatabase? = null
+
+    constructor(context : Context){
+        var db = DataBaseHelperNotes(context)
+        sqlDB = db.writableDatabase
+    }
+
+    inner class DataBaseHelperNotes : SQLiteOpenHelper {
+        var context : Context? = null
+        constructor(context : Context) : super( context, dbName, null, dbVersion ) {
+            this.context = context
+        }
+
+        override fun onCreate(db: SQLiteDatabase?) {
+            db!!.execSQL(queryCreateTable)
+            Toast.makeText(this.context, "DB Created", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            db!!.execSQL("DROP TABLE IF EXISTS " + dbTable)
+            //check the dbVersion...              dbVersion = newVersion
+        }
+
+    }
+
+    fun insertNote(values: ContentValues):Long{
+        val ID = sqlDB!!.insert(dbTable, "", values)
+        return ID
+    }
+    
+    //specifying which columns you want, the rows and also in which order to sort the results
+    fun queryDB (projection: Array<String>, )
+}
+
